60dc058a7ea35f14f70cca9921089f6fd0d57aea
Clawchette <marie.gonzalez@ynov.com>
2021-02-22 15:32:54 +0100

    formulaire recherche image front end (sans tags)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/app.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
     <script src="./js/app.js"></script>
 
@@ -15,8 +15,10 @@
 <body>
     @include('header')
 
+    <div class="decalage">
     @yield('content')
-    
+    </div>
+
     @include('footer')
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
